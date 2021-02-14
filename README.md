02facff36b1f5c411a2406d4afc69c375aa81da8
Bokie Tarathep <tarathec@ais.co.th>
2021-02-14 12:03:02 +0700

    init project

--- README.md
+++ README.md
@@ -22,3 +22,28 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+---
+
+## Init Project
+```
+vue create vue=js-client-crud
+```
+- select vue2 default (babel,eslint)
+- other vue2 or vueX
+
+### Install vue-router
+```
+npm install vue-router
+```
+- create file router.js in src
+
+### Install axios
+```
+npm install axios
+```
+- src/main.js call vue router
+
+
+
+
--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- Add Nav Bar boostap -->
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+      <!-- router link to index -->
+      <router-link to="/" class="navbar-brand">Bokie</router-link>
+      <div class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <!-- router link to /tutorials -->
+          <router-link to="/tutorials" class="nav-link">Tutorials</router-link>
+        </li>
+        <li class="nav-item">
+          <!-- router link to /add -->
+          <router-link to="/add" class="nav-link">Add</router-link>
+        </li>
+      </div>
+    </nav>
+
+    <div class="container mt-3">
+      <router-view />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
